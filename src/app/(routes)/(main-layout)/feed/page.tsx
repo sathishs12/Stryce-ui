db2236b04b02225e82367013783a9acb220173c1
sathishs12 <sathish638236@gmail.com>
2026-04-04 20:03:27 +0530

    fix:feed page issue

--- src/app/(routes)/(main-layout)/feed/page.tsx
+++ src/app/(routes)/(main-layout)/feed/page.tsx
@@ -2,8 +2,9 @@ import FeedTemplate from "@/modules/feed/templates";
 import { Suspense } from "react";
 
 export default function FeedPage() {
-  return 
-  <Suspense fallback={null}>
-    <FeedTemplate />
-    </Suspense>;
+  return (
+    <Suspense fallback={null}>
+      <FeedTemplate />
+    </Suspense>
+  );
 }
